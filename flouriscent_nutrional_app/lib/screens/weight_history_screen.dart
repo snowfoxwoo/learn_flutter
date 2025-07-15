bdaca5fb4832093f06c52a112bed0f0ae0af028f
rafaelngg <RafaelNg117@gmail.com>
2025-07-15 15:45:41 +0800

    Change UI of weigh_tracker

--- flouriscent_nutrional_app/lib/screens/weight_history_screen.dart
+++ flouriscent_nutrional_app/lib/screens/weight_history_screen.dart
@@ -116,7 +116,7 @@ class WeightHistoryScreen extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black.withValues(alpha: 0.05),
             blurRadius: 10,
             offset: const Offset(0, 5),
           ),
@@ -159,7 +159,7 @@ class WeightHistoryScreen extends StatelessWidget {
         Container(
           padding: const EdgeInsets.all(8),
           decoration: BoxDecoration(
-            color: color.withOpacity(0.1),
+            color: color.withValues(alpha: 0.1),
             borderRadius: BorderRadius.circular(8),
           ),
           child: Icon(icon, color: color, size: 20),
@@ -198,7 +198,7 @@ class WeightHistoryScreen extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.05),
+            color: Colors.black.withValues(alpha: 0.05),
             blurRadius: 5,
             offset: const Offset(0, 2),
           ),
